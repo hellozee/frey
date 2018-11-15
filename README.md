a313a4cdf41ddb67b74724fd563c36e2d3fb83eb
hellozee <hellozee@disroot.org>
2018-11-15 14:48:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A simple skeleton for my C++ projects
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int 
+main(int argv, char *argc[]) 
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
