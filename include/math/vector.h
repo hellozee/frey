eabbbe76e947cffe58e199a2493a15b45ea1152a
hellozee <hellozee@disroot.org>
2018-11-15 23:55:27 +0530

    Overloaded the operators of vector class

--- include/math/vector.h
+++ include/math/vector.h
@@ -3,16 +3,27 @@
 
 namespace frey{
     class vector{
-        public:
+        private:
             double x,y,z;
-
+        public:
             vector();
             vector(double val);
             vector(double i, double j, double k);
+            
+            inline double getx() const { return x; }
+            inline double gety() const { return y; }
+            inline double getz() const { return z; }
 
             double magnitude();
             vector normalize();
 
+            vector operator+(const vector v);
+            vector operator!();
+            vector operator-(const vector v);
+            double operator*(const vector v);
+            vector operator^(const vector v);
+            void operator=(const vector v);
+            bool operator==(const vector v) const;
     };
 }
 
--- src/math/vector.cpp
+++ src/math/vector.cpp
@@ -4,21 +4,15 @@
 
 frey::vector::vector():
     x(1.0),y(1.0),z(1.0)
-{
-
-}
+{ }
 
 frey::vector::vector(double val):
     x(val), y(val), z(val)
-{
-
-}
+{ }
 
 frey::vector::vector(double i, double j, double k):
     x(i),y(j),z(k)
-{
-
-}
+{ }
 
 double 
 frey::vector::magnitude()
@@ -35,4 +29,51 @@ frey::vector::normalize()
     int tz = z / mag;
 
     return vector(tx,ty,tz);
+}
+
+frey::vector
+frey::vector::operator+(const frey::vector v)
+{
+    return frey::vector(x+v.getx(), y+v.gety(), z+v.getz());
+}
+
+frey::vector
+frey::vector::operator!()
+{
+    return frey::vector(-x,-y,-z);
+}
+
+frey::vector
+frey::vector::operator-(const frey::vector v)
+{
+    return frey::vector(x-v.getx(), y-v.gety(), z-v.getz());
+}
+
+double
+frey::vector::operator*(const frey::vector v)
+{
+    return x*v.getx() + y*v.gety() + z*v.getz();
+}
+
+frey::vector
+frey::vector::operator^(const frey::vector v)
+{
+    double tx = y * v.getz() - z * v.gety();
+    double ty = z * v.getx() - x * v.getz();
+    double tz = x * v.gety() - y * v.getx();
+    return frey::vector(tx,ty,tz);
+}
+
+void
+frey::vector::operator=(const frey::vector v)
+{
+    x = v.getx();
+    y = v.gety();
+    z = v.getz();
+}
+
+bool
+frey::vector::operator==(const frey::vector v) const 
+{
+    return x == v.getx() && y == v.gety() && z == v.getz();
 }
