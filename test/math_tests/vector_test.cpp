496cf1ccbcd51db5df369f0cf2cd57224ccb2656
hellozee <hellozee@disroot.org>
2018-11-15 23:55:43 +0530

    Added tests for the operators

--- test/math_tests/vector_test.cpp
+++ test/math_tests/vector_test.cpp
@@ -4,19 +4,12 @@
 TEST(VectorTest, TestConstructors)
 {
     frey::vector empty;
-    EXPECT_EQ(empty.x, 1.0);
-    EXPECT_EQ(empty.y, 1.0);
-    EXPECT_EQ(empty.z, 1.0);
 
     frey::vector single_val(1.0);
-    EXPECT_EQ(single_val.x, 1.0);
-    EXPECT_EQ(single_val.y, 1.0);
-    EXPECT_EQ(single_val.z, 1.0);
+    EXPECT_EQ(single_val, empty);
 
     frey::vector multi_val(1.0,1.0,1.0);
-    EXPECT_EQ(multi_val.z, 1.0);
-    EXPECT_EQ(multi_val.z, 1.0);
-    EXPECT_EQ(multi_val.z, 1.0);
+    EXPECT_EQ(multi_val, empty);
 }
 
 TEST(VectorTest, TestMagnitude)
@@ -29,9 +22,25 @@ TEST(VectorTest, TestNormalize)
 {
     frey::vector temp_vector(1.0,0.0,0.0);
     frey::vector test_vector = temp_vector.normalize();
-    EXPECT_EQ(test_vector.x, 1.0);
-    EXPECT_EQ(test_vector.y, 0.0);
-    EXPECT_EQ(test_vector.z, 0.0);
+    EXPECT_EQ(test_vector, temp_vector);
+}
+
+TEST(VectorTest, TestOperators)
+{
+    frey::vector v1(1.0,1.0,1.0), v2(1.0,1.0,1.0);
+    EXPECT_EQ(v1,v2);
+
+    frey::vector add_result = v1 + v2;
+    EXPECT_EQ(add_result, frey::vector(2.0,2.0,2.0));
+
+    frey::vector negation_result = v1 + (!v2);
+    EXPECT_EQ(negation_result, frey::vector(0.0,0.0,0.0));
+
+    double dot_result = v1 * v2;
+    EXPECT_EQ(dot_result, 3.0);
+
+    frey::vector cross_result = v1^v2;
+    EXPECT_EQ(cross_result, frey::vector(0.0,0.0,0.0));
 }
 
 int 
